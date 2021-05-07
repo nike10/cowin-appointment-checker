21e3180950c0df3329543ccfa7f106ee63e263f1
nike10 <58499539+nike10@users.noreply.github.com>
2021-05-07 17:41:07 +0530

    Update test3.py

--- test3.py
+++ test3.py
@@ -12,14 +12,18 @@ import json
 import streamlit as st
 import pandas as pd
 from datetime import datetime
+import os
+
+
 st.set_page_config( layout='wide')
 @st.cache(allow_output_mutation=True)
 def sel():
-    options = webdriver.ChromeOptions()
-    options.add_argument('--ignore-certificate-errors')
-    options.add_argument('--incognito')
-    options.add_argument('--headless')
-    driver = webdriver.Chrome(r'cowin-appointment-checker/chromedriver',chrome_options=options) 
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
     return driver
 
 @st.cache(allow_output_mutation=True)
